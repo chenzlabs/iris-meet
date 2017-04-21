c9d02c1a32d4d937edd03478f4a4ad7ac184503d
machenmusik <machenmusik@gmail.com>
2017-04-21 15:00:55 -0400

    flatten remote participants so all of them show up

--- src/components/horizontal-wrapper/index.js
+++ src/components/horizontal-wrapper/index.js
@@ -2,13 +2,23 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './horizontal-wrapper.css'
 
-const HorizontalWrapper = ({isHidden, children}) => (
+const HorizontalWrapper = ({isHidden, children}) => {
+  var flattened = [];
+  children.forEach((child) => {
+    if (Array.isArray(child)) {
+      child.forEach((el) => { flattened.push(el); });
+    } else {
+      flattened.push(child);
+    }
+  });
+  return (
   <div id="footer">
     <div className={isHidden ? "horizontal-wrapper videoBarHide" : "horizontal-wrapper videoBarShow"}>
-        {children}
+      {flattened}
     </div>
   </div>
-);
+  );
+}
 
 HorizontalWrapper.PropTypes = {
   isHidden: PropTypes.bool.isRequired,
